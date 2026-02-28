5daeceb106038cb874dd91d0f23ca89ae4bbfb68
demial003 <demial003@gmail.com>
2026-02-28 18:05:58 -0500

    succesfully display image to terminal

--- main.c
+++ main.c
@@ -8,37 +8,52 @@
 #include "stb_image_write.h"
 
 int main(void) {
-  int x, y, n;
-  unsigned char *data = stbi_load("arch.png", &x, &y, &n, 0);
-  unsigned char *new_data = malloc(300);
-  unsigned char *final_data = malloc(100);
-  int err = stbir_resize_uint8(data, x, y, 0, new_data, 10, 10, 0, n);
+  int initial_width, initial_height, componenets;
+  unsigned char *data = stbi_load("triangle.png", &initial_width,
+                                  &initial_height, &componenets, 0);
+  int scaled_width = initial_width / 31;
+  int scaled_height = initial_height / 31;
+  unsigned char *scaled_data =
+      malloc(scaled_width * scaled_height * componenets);
+  unsigned char *final_data = malloc(scaled_width * scaled_height);
+  int err =
+      stbir_resize_uint8(data, initial_width, initial_height, 0, scaled_data,
+                         scaled_width, scaled_height, 0, componenets);
   if (data == NULL) {
     fprintf(stderr, "failed to open image");
     exit(EXIT_FAILURE);
   }
 
-  if (err != 1) {
-    fprintf(stderr, "failed to open image");
-    exit(EXIT_FAILURE);
-  }
-
   char *chars =
-      "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+      "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@";
   int len = strlen(chars);
   char stuff[len];
   for (int i = 0; i < len; i++) {
     stuff[i] = chars[i];
   }
-  printf("width: %d height: %d componenets %d\n", x, y, n);
+  printf("width: %d height: %d componenets %d\n", scaled_width, scaled_height,
+         componenets);
   int j = 0;
-  for (int i = n; i < 300; i += n) {
-    final_data[j] = data[i - 3] + 65;
+  char *output = malloc(initial_width * initial_height);
+  for (int i = componenets; i < scaled_width * scaled_height * 3;
+       i += componenets) {
+    final_data[j] =
+        (scaled_data[i - 3] + scaled_data[i - 2] + scaled_data[i]) / 3;
+    int idx = (final_data[j] >> 2);
+    output[j] = stuff[idx];
     j++;
   }
-  stbi_write_png("arch2.png", 10, 10, 1, final_data, 10);
+  for (int i = 0; i < scaled_width * scaled_height; i++) {
+    printf("%c", output[i]);
+    printf("%c", output[i]);
+    // printf("%c", output[i]);
+    if (i % scaled_width == 0) {
+      printf("\n");
+    }
+  }
   stbi_image_free(data);
-  free(new_data);
+  free(output);
+  free(scaled_data);
   free(final_data);
 }
 
