5a5ec2a6210615185e747811975c10f34b09dd66
demial003 <demial003@gmail.com>
2026-03-09 15:45:48 -0400

    fix off by one error

--- main.c
+++ main.c
@@ -9,28 +9,40 @@
 #include "stb_image_write.h"
 
 unsigned char average_rgb(unsigned char *data, int index) {
-  return (data[index - 2] + data[index - 1] + data[index]) / 3;
+  return (data[index + 2] + data[index + 1] + data[index]) / 3;
 }
 
 unsigned char min_max_rgb(unsigned char *data, int index) {
-  unsigned char r = data[index - 2];
-  unsigned char g = data[index - 1];
-  unsigned char b = data[index];
 
+  unsigned char r = data[index + 2];
+  unsigned char g = data[index + 1];
+  unsigned char b = data[index];
   return (max(r, g, b) + min(r, g, b)) / 2;
 }
+
+unsigned luminosity_rgb(unsigned char *data, int index) {
+  unsigned char r = data[index + 2];
+  unsigned char g = data[index + 1];
+  unsigned char b = data[index];
+
+  unsigned char r_new = (unsigned char)(0.21 * r);
+  unsigned char g_new = (unsigned char)(0.72 * g);
+  unsigned char b_new = (unsigned char)(0.07 * b);
+
+  return r_new + g_new + b_new;
+}
 int main(void) {
-  int initial_width, initial_height, componenets;
+  int initial_width, initial_height, components;
   unsigned char *data = stbi_load("triangle.png", &initial_width,
-                                  &initial_height, &componenets, 0);
-  int scaled_width = initial_width / 10;
-  int scaled_height = initial_height / 10;
+                                  &initial_height, &components, 0);
+  int scaled_width = initial_width / 10 + 1;
+  int scaled_height = initial_height / 10 + 1;
   unsigned char *scaled_data =
-      malloc(scaled_width * scaled_height * componenets);
+      malloc(scaled_width * scaled_height * components);
   unsigned char *final_data = malloc(scaled_width * scaled_height);
   int err =
       stbir_resize_uint8(data, initial_width, initial_height, 0, scaled_data,
-                         scaled_width, scaled_height, 0, componenets);
+                         scaled_width, scaled_height, 0, components);
   if (data == NULL) {
     fprintf(stderr, "failed to open image");
     exit(EXIT_FAILURE);
@@ -44,31 +56,23 @@ int main(void) {
     stuff[i] = chars[i];
   }
 
-  printf("width: %d height: %d componenets %d\n", scaled_width, scaled_height,
-         componenets);
+  printf("width: %d height: %d components %d\n", scaled_width, scaled_height,
+         components);
 
   int j = 0;
-  char *output = malloc(initial_width * initial_height);
-  for (int i = componenets - 1; i < scaled_width * scaled_height * 3;
-       i += componenets) {
-    final_data[j] = min_max_rgb(scaled_data, i);
-    int idx = (final_data[j] >> 2);
-    output[j] = stuff[idx];
-    if (idx > 25) {
-      output[j] = ' ';
-    } else {
-      output[j] = stuff[idx];
-    }
-    j++;
-  }
-
-  printf("\n");
+  char *output = malloc(scaled_width * scaled_height);
   for (int i = 0; i < scaled_width * scaled_height; i++) {
-    printf("%c", output[i]);
-    printf("%c", output[i]);
-    if (i % scaled_width == 0) {
+    int pixel_pos = i * components;
+    final_data[j] = min_max_rgb(scaled_data, pixel_pos);
+
+    int idx = (final_data[j] >> 2);
+    printf("%c%c", stuff[idx], stuff[idx]);
+
+    if ((i + 1) % scaled_width == 0) {
       printf("\n");
     }
+
+    ++j;
   }
 
   printf("\n");
